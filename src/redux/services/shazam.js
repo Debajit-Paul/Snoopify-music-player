4602f6cd05b30be16ad612d4574b4094fef2bab5
Debajit Paul <debajitfan.of@gmail.com>
2023-04-22 18:38:15 +0530

    Update shazam.js

--- src/redux/services/shazam.js
+++ src/redux/services/shazam.js
@@ -3,18 +3,49 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const shazamApi = createApi({
   reducerPath: "shazamApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam.p.rapidapi.com",
+    baseUrl: "https://shazam-core.p.rapidapi.com",
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
-        "0f75bfb36fmshd909384fb8f43e0p166d68jsn506fdd1fa4e5"
+        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
       );
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => "/charts/track" }),
+    getTopCharts: builder.query({ query: () => "/v1/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/v1/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/v1/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/v2/artists/details?artist_id=${artistId}`,
+    }),
+    getArtistTopSongs: builder.query({
+      query: (artistId) => `/artists/get-top-songs?id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/v1/search/multi?query=${searchTerm}&search_type=SONGS_ARTISTS`,
+      // `/v1/search/multi?search_type=SONG_ARTISTS&query=${searchTerm}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamApi;
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+} = shazamApi;
